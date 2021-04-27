3155713a0ae6e8077148918436dec6646615458c
mihail <mihail.karavatsky@mojam.co>
2021-04-27 15:15:03 +0300

    Added routes match for proxying

---
+++ enums/routes_match.js
@@ -0,0 +1,83 @@
+module.exports = Object.freeze({
+    'GetInv': {
+        route: 'my-inventory'
+    },
+    'Trades': {
+        route: 'trades'
+    },
+    'GetMoney': {
+        route: 'get-money'
+    },
+    'PingPong': {
+        route: 'ping'
+    },
+    'GoOffline': {
+        route: 'go-offline'
+    },
+    'GetToken': {
+        route: null
+    },
+    'SetToken': {
+        route: null
+    },
+    'GetWSAuth': {
+        route: 'get-ws-auth'
+    },
+    'UpdateInventory': {
+        route: 'update-inventory'
+    },
+    'itemsInCache': {
+        route: null
+    },
+    'GetDiscounts': {
+        route: null
+    },
+    'GetCounters': {
+        route: null
+    },
+    'GetMySellOffers': {
+        route: null
+    },
+
+
+
+    'ItemHistory': {
+        route: 'my-inventory'
+    },
+    'Trades': {
+        route: 'trades'
+    },
+    'GetMoney': {
+        route: 'get-money'
+    },
+    'PingPong': {
+        route: 'ping'
+    },
+    'GoOffline': {
+        route: 'go-offline'
+    },
+    'GetToken': {
+        route: null
+    },
+    'SetToken': {
+        route: null
+    },
+    'GetWSAuth': {
+        route: 'get-ws-auth'
+    },
+    'UpdateInventory': {
+        route: 'update-inventory'
+    },
+    'itemsInCache': {
+        route: null
+    },
+    'GetDiscounts': {
+        route: null
+    },
+    'GetCounters': {
+        route: null
+    },
+    'GetMySellOffers': {
+        route:
+    },
+})
