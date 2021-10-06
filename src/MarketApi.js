887590256c63b92b32742a18bdbe588a8542172c
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2021-10-06 16:14:58 +0500

    Added for buyV2CreateFor

--- src/MarketApi.js
+++ src/MarketApi.js
@@ -1550,7 +1550,7 @@ class MarketApi {
         if(typeof item === 'object') {
             params.id = self.formatItem(item);
         } else {
-            params.market_hash_name = item;
+            params.hash_name = item;
         }
 
         if(tradeData && tradeData.partnerId && tradeData.tradeToken) {
